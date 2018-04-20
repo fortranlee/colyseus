beca55c94eec0e5fe99a27cd1f5933bebb0467ce
fortran <lihuiquan@email.szu.edu.cn>
2018-04-20 17:18:11 +0800

    added roomId

--- src/Room.ts
+++ src/Room.ts
@@ -176,7 +176,7 @@ export abstract class Room<T= any> extends EventEmitter {
   }
 
   public send(client: Client, data: any): void {
-    send(client, [ Protocol.ROOM_DATA, data ]);
+    send(client, [ this.roomId, Protocol.ROOM_DATA, data ]);
   }
 
   public broadcast(data: any, options?: BroadcastOptions): boolean {
@@ -187,7 +187,7 @@ export abstract class Room<T= any> extends EventEmitter {
 
     // encode all messages with msgpack
     if (!(data instanceof Buffer)) {
-      data = msgpack.encode([Protocol.ROOM_DATA, data]);
+      data = msgpack.encode([this.roomId, Protocol.ROOM_DATA, data]);
     }
 
     let numClients = this.clients.length;
@@ -224,6 +224,7 @@ export abstract class Room<T= any> extends EventEmitter {
 
   protected sendState(client: Client): void {
     send(client, [
+      this.roomId,
       Protocol.ROOM_STATE,
       this._previousStateEncoded,
       this.clock.currentTime,
@@ -271,7 +272,7 @@ export abstract class Room<T= any> extends EventEmitter {
     this._previousStateEncoded = currentStateEncoded;
 
     // broadcast patches (diff state) to all clients,
-    return this.broadcast( msgpack.encode([ Protocol.ROOM_STATE_PATCH, patches ]) );
+    return this.broadcast( msgpack.encode([ this.roomId, Protocol.ROOM_STATE_PATCH, patches ]) );
   }
 
   protected _disposeIfEmpty() {
@@ -350,7 +351,7 @@ export abstract class Room<T= any> extends EventEmitter {
     }
 
     // confirm room id that matches the room name requested to join
-    send(client, [ Protocol.JOIN_ROOM, client.sessionId ]);
+    send(client, [ this.roomId, Protocol.JOIN_ROOM, client.sessionId ]);
 
     // emit 'join' to room handler
     this.emit('join', client);
